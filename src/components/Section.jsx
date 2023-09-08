74c6c083e37c426b3ee7ad735275075cfbd114b1
kuanchiliao1 <kuanchiliao@gmail.com>
2023-09-08 16:43:34 -0700

    Add title and set up form crud

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,22 +1,23 @@
 import {useState} from 'react'
 
-export default function Section({ children, crudEnabled=false}) {
-  const [forms, setForms] = useState([])
+export default function Section({ children, crudEnabled=false, title}) {
+  const [extraForms, setExtraForms] = useState([])
 
   function handleAddForm() {
-    setForms(prevForms => [...prevForms, children])
+    setExtraForms(prevForms => [...prevForms, children])
   }
 
   function handleRemoveForm() {
-    setForms(prevForms => [...prevForms])
+    setExtraForms(prevForms => prevForms.filter((form, index) => index < prevForms.length - 1 ))
   }
 
   return (
     <div className="section">
+      <h2>{title}</h2>
       {crudEnabled && <button onClick={() => handleAddForm()}>Add</button>}
-      {children}
-      {forms}
       {crudEnabled && <button onClick={() => handleRemoveForm()}>Remove</button>}
+      {children}
+      {extraForms}
     </div>
   )
 }
