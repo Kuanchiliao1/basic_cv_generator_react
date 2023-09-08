f7a4e0273dad22e00171daa2e225941683f45504
kuanchiliao1 <kuanchiliao@gmail.com>
2023-09-08 12:34:23 -0700

    Wrap Form in Section

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,26 @@ import './App.css'
 import Section from './components/Section'
 import Form from './components/forms/Form'
 import BasicTextInput from './components/inputs/BasicTextInput'
+import ListInput from './components/inputs/ListInput'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Form
-        title='General Info'
-      >
-        <BasicTextInput type='text' label='name' />
-        <BasicTextInput type='text' label='email' />
-        <BasicTextInput stuff='something' type='tel' label='phone' />
-      </Form>
+      <Section>
+        <Form
+          className='form'
+          title='General Info'
+        >
+          <BasicTextInput type='text' labelName='Name' />
+          <BasicTextInput type='text' labelName='Email' />
+          <BasicTextInput stuff='something' type='tel' labelName='Phone' />
+          <ListInput>
+            {['testing', 'stuff']}
+          </ListInput>
+        </Form>
+      </Section>
       {/* <Section>
         <Form>
           <Input />
