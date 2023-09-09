146906599810ec6b6a3e0fffdd0864cb93a347e1
kuanchiliao1 <kuanchiliao@gmail.com>
2023-09-09 08:03:59 -0700

    Create handleInputChange()

--- src/components/inputs/BasicTextInput.jsx
+++ src/components/inputs/BasicTextInput.jsx
@@ -3,14 +3,18 @@ import { allFormsDataContext } from "../../App"
 import { v4 as uuidv4 } from "uuid"
 
 export default function BasicTextInput({labelName, ...rest}) {
-  let value = useContext(allFormsDataContext)
+  let {allFormsData, setAllFormsData} = useContext(allFormsDataContext)
   let id = uuidv4()
   // {"general_info":{},"education":[{}],"experience":[{}]}
+
+  const handleInputChange = (e) => {
+    setAllFormsData(prevData => ({...prevData, general_info: {name: e.target.value}}))
+  }
 
   return (
     <>
       <label htmlFor={id}>{labelName}</label>
-      <input type="text" name={id} {...rest} />
+      <input type="text" name={id} value={allFormsData['general_info'].name} onChange={handleInputChange} {...rest} />
     </>
   )
 }
