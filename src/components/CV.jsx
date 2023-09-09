5b2042a8fab8f977ed8d217a043c3c9386ccc877
kuanchiliao1 <kuanchiliao@gmail.com>
2023-09-09 08:03:20 -0700

    Test to grab form info from context

--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -1,7 +1,14 @@
+import { useContext } from "react"
+import { allFormsDataContext } from "../App"
+
 export default function CV() {
+  // {"general_info":{},"education":[{}],"experience":[{}]}
+  let {allFormsData} = useContext(allFormsDataContext)
+  console.log(allFormsData['general_info'].name)
+  
   return (
     <div className='cv-container'>
-      kj
+      {allFormsData['general_info'].name}
     </div>
   )
 }
