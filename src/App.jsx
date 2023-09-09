b67b77920c165d578972bbe212c812ac29d67d74
kuanchiliao1 <kuanchiliao@gmail.com>
2023-09-08 17:17:15 -0700

    Create context

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,55 @@
-import { useState } from 'react'
+import { useState, createContext } from 'react'
 import './App.css'
 import Section from './components/Section'
 import Form from './components/forms/Form'
 import BasicTextInput from './components/inputs/BasicTextInput'
 import ListInput from './components/inputs/ListInput'
 import DateRange from './components/inputs/DateRange'
+import CV from './components/CV'
+
+let allFormsDataContext = createContext()
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <Section  title='General Info'>
-        <Form
-          className='form'
-          title='General Info'
-        >
-          <BasicTextInput type='text' labelName='Name' />
-          <BasicTextInput type='text' labelName='Email' />
-          <BasicTextInput stuff='something' type='tel' labelName='Phone' />
-          <ListInput>
-            {['testing', 'stuff']}
-          </ListInput>
-        </Form>
-      </Section>
-      <Section crudEnabled={true} title='Education'>
-        <Form
-          className='form'
-          title='Education'
-        >
-          <BasicTextInput type='text' labelName='School name' />
-          <DateRange></DateRange>
-        </Form>
-      </Section>
-      <Section crudEnabled={true} title='Experience'>
-        <Form>
-          <BasicTextInput type='text' labelName='Company name' />
-          <BasicTextInput type='text' labelName='Job title' />
-          <DateRange></DateRange>
-        </Form>
-      </Section>
+    <allFormsDataContext.Provider value={{general_info: {}, education: [{}], experience: [{}]}}>
+      <div className='main-container'>
+        <div className="section-container">
+          <Section  title='General Info'>
+            <Form
+              className='form'
+              title='General Info'
+            >
+              <BasicTextInput type='text' labelName='Name' />
+              <BasicTextInput type='text' labelName='Email' />
+              <BasicTextInput stuff='something' type='tel' labelName='Phone' />
+              <ListInput>
+                {['testing', 'stuff']}
+              </ListInput>
+            </Form>
+          </Section>
+          <Section crudEnabled={true} title='Education'>
+            <Form
+              className='form'
+              title='Education'
+            >
+              <BasicTextInput type='text' labelName='School name' />
+              <DateRange></DateRange>
+            </Form>
+          </Section>
+          <Section crudEnabled={true} title='Experience'>
+            <Form>
+              <BasicTextInput type='text' labelName='Company name' />
+              <BasicTextInput type='text' labelName='Job title' />
+              <DateRange></DateRange>
+            </Form>
+          </Section>
+        </div>
+        <CV>
+        
+        </CV>
+      </div>
       {/* <Section>
         <Form>
           <Input />
@@ -56,8 +66,9 @@ function App() {
           
         </ExperienceForm>
       </Section> */}
-    </>
+    </allFormsDataContext.Provider>
   )
 }
 
 export default App
+export { allFormsDataContext }
