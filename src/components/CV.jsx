ba1c993820f22d5c5eb8ac7e7585fecedd35c129
kuanchiliao1 <kuanchiliao@gmail.com>
2023-09-13 12:50:47 -0700

    Add const for school_name

--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -4,13 +4,15 @@ import { allFormsDataContext } from "../App"
 export default function CV() {
   // {"general_info":{},"education":[{}],"experience":[{}]}
   let {allFormsData} = useContext(allFormsDataContext)
-  const {name, email, phone} = allFormsData.general_info
+  const {name, email, phone} = allFormsData.general_info[0]
+  const {school_name} = allFormsData.education[0]
   
   return (
     <div className='cv-container'>
       <p>{name}</p>
       <p>{email}</p>
       <p>{phone}</p>
+      <p>{school_name}</p>
     </div>
   )
 }
