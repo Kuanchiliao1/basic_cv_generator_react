a968f8fc58403325ab34a6e3547e3f3bbd74ef4e
kuanchiliao1 <kuanchiliao@gmail.com>
2023-09-13 12:50:07 -0700

    Format with prettier

--- src/components/inputs/BasicTextInput.jsx
+++ src/components/inputs/BasicTextInput.jsx
@@ -1,31 +1,48 @@
-import { useContext } from "react"
-import { allFormsDataContext } from "../../App"
-import { v4 as uuidv4 } from "uuid"
-import toSnakeCase from "../../utilities/toSnakeCase"
+import { useContext } from "react";
+import { allFormsDataContext } from "../../App";
+import { v4 as uuidv4 } from "uuid";
+import toSnakeCase from "../../utilities/toSnakeCase";
 
-export default function BasicTextInput({ labelName, title, ...rest }) {
-  let { allFormsData, setAllFormsData } = useContext(allFormsDataContext)
-  let id = uuidv4()
-  const sectionKey = toSnakeCase(title || '')
-  let inputKey = toSnakeCase(labelName)
-  console.log({ inputKey })
-  console.log({ title })
-  // {"general_info":{},"education":[{}],"experience":[{}]}
+export default function BasicTextInput({
+  labelName,
+  title,
+  formIndex = 0,
+  ...rest
+}) {
+  let { allFormsData, setAllFormsData } = useContext(allFormsDataContext);
+  let id = uuidv4();
+  const sectionKey = toSnakeCase(title || "");
+  let inputKey = toSnakeCase(labelName);
+  // {"general_info":[{}],
+  // "education":[{}],
+  // "experience":[{}]}
 
   const handleInputChange = (e) => {
-    setAllFormsData(prevData => (
-      {
+    setAllFormsData((prevData) => {
+      return {
         ...prevData,
-        [sectionKey]:
-          { ...prevData[sectionKey], [inputKey]: e.target.value }
-      }
-    ))
-  }
+        [sectionKey]: Object.assign([], prevData[sectionKey], {
+          [formIndex]: {
+            ...prevData[sectionKey][formIndex],
+            [inputKey]: e.target.value,
+          },
+        }),
+      };
+    });
+    console.log(e.target.value);
+    console.log(allFormsData);
+  };
 
   return (
     <>
       <label htmlFor={id}>{labelName}</label>
-      <input type="text" name={id} value={allFormsData['general_info'][inputKey]} onChange={handleInputChange} {...rest} />
+      <input
+        type="text"
+        name={id}
+        value={allFormsData["general_info"][inputKey]}
+        onChange={handleInputChange}
+        {...rest}
+      />
     </>
-  )
+  );
 }
