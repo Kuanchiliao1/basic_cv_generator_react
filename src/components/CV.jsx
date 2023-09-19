fd1f16139389a0eaf3dda3a4158fe5a853881f3f
kuanchiliao1 <kuanchiliao@gmail.com>
2023-09-19 09:30:31 -0700

    Include text labels for dev

--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -6,13 +6,15 @@ export default function CV() {
   let {allFormsData} = useContext(allFormsDataContext)
   const {name, email, phone} = allFormsData.general_info[0]
   const {school_name} = allFormsData.education[0]
+  const {school_name1} = allFormsData.education[1] || {}
   
   return (
     <div className='cv-container'>
-      <p>{name}</p>
-      <p>{email}</p>
-      <p>{phone}</p>
-      <p>{school_name}</p>
+      <p>{`Name: ${name || ''}`}</p>
+      <p>{`Email: ${email || ''}`}</p>
+      <p>{`Phone: ${phone || ''}`}</p>
+      <p>{`School name: ${school_name}`}</p>
+      <p>{`School name2: ${school_name1}`}</p>
     </div>
   )
 }
