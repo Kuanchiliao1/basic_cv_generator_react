56066bf49f0afe18a2f9a75d5e56df5466016acd
kuanchiliao1 <kuanchiliao@gmail.com>
2023-09-19 10:34:19 -0700

    Update to change allFormsData context

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,17 +1,39 @@
-import {useState, cloneElement} from 'react'
+import {useState, cloneElement, useContext} from 'react'
+import { allFormsDataContext } from "../App";
+import toSnakeCase from '../utilities/toSnakeCase';
 
 export default function Section({ children, crudEnabled=false, title}) {
   const [extraForms, setExtraForms] = useState([])
+  const sectionKey = toSnakeCase(title || "");
+  let { setAllFormsData } = useContext(allFormsDataContext);
+
 
   function handleAddForm() {
     const formIndex = extraForms.length + 1
     const form = cloneElement(children, {formIndex: formIndex})
-    console.log('children', children)
     setExtraForms(prevForms => [...prevForms, form])
+
+    setAllFormsData((prevData) => {
+      console.log('allFormsData', {
+        ...prevData,
+        [sectionKey]: Object.assign([], prevData[sectionKey], {[formIndex]: {}})
+      })
+      return {
+        ...prevData,
+        [sectionKey]: Object.assign([], prevData[sectionKey], {formIndex: undefined})
+      }
+    })
   }
 
   function handleRemoveForm() {
     setExtraForms(prevForms => prevForms.filter((form, index) => index < prevForms.length - 1 ))
+
+    setAllFormsData((prevData) => {
+      return {
+        ...prevData,
+        [sectionKey]: Object.assign([], prevData[sectionKey].slice(0, -1))
+      }
+    })
   }
 
   return (
